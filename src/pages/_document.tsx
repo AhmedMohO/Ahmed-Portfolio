4a7991b64889157ec109e7941c9bcca4687fab22
BadHunterN1 <ahmam2004@gmail.com>
2025-06-15 19:09:49 +0300

    ....

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,13 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta charSet="UTF-8" />
 				<meta
 					name="description"
 					content="🚀Explore my projects, skills, and creative journey — all in one place"
 				/>
+				<meta content="Ahmed's Portfolio" property="og:site_name" />
+				<meta property="og:image" content="/My_Icon.jpg" />
 
 				<meta
 					itemProp="description"
@@ -35,8 +38,8 @@ export default function Document() {
 
 				<meta name="theme-color" content="#000000" />
 				<meta property="og:type" content="website" />
-				<link rel="icon" type="image/png" href="/My_Icon.jpg" />
 
+				<link rel="icon" type="image/jpg" href="/My_Icon.jpg" />
 				<link rel="preconnect" href="https://cdn.sanity.io" />
 				<link rel="dns-prefetch" href="https://cdn.sanity.io" />
 			</Head>
