1f66b1ebcac0572ba808292df94de1cc40f9a387
BadHunterN1 <ahmam2004@gmail.com>
2025-06-15 22:36:32 +0300

    ..

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,6 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<meta charSet="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 				<meta name="title" content="Ahmed's Portfolio" />
 				<meta
 					name="description"
